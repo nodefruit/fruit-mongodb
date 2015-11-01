741149a9632d266786290866096a128844f22ad6
Khalid REHIOUI <Rehioui@gmail.com>
2015-11-01 14:32:13 +0000

    Final travis configuration

--- test/testEnv.js
+++ test/testEnv.js
@@ -7,11 +7,10 @@ if(process.env.HOST && process.env.DB && process.env.PORT) {
     , port                : process.env.PORT
   }
 } else {
-  console.log(' You need to specify database information as environment variables ');
   console.log(' Make sure to specify a new and empty database for test            ');
   console.log(' example :                                                         ');
   console.log(' HOST="localhost" DB="test" PORT="27017" npm test                  ');
-  throw new TypeError('');
+  throw new TypeError('You need to specify database information as environment variables');
 }
 
 module.exports = config;
